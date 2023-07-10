7f9258414cc11f4276825fda9080affb20e92960
codingjamee <codingjamee@gmail.com>
2023-07-10 14:09:37 +0900

    응답 dummy json array title과 results로 분리

--- controllers/cate.js
+++ controllers/cate.js
@@ -1,13 +1,158 @@
-exports.getPosts = (req, res, next) => {
+exports.getBest = (req, res, next) => {
   res.status(200).json({
+    title: "베스트",
     results: [
       {
         card__imgUrl: "https://picsum.photos/id/233/350/350",
-        card_title: "타이틀1",
+        card_title: "베스트1",
       },
       {
         card__imgUrl: "https://picsum.photos/id/253/350/350",
-        card_title: "타이틀2",
+        card_title: "베스트2",
+      },
+    ],
+  });
+};
+
+exports.getNew = (req, res, next) => {
+  res.status(200).json({
+    title: "신상품",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "신상품1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "신상품2",
+      },
+    ],
+  });
+};
+
+exports.getBasic = (req, res, next) => {
+  res.status(200).json({
+    title: "베이직",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "베이직1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "베이직2",
+      },
+    ],
+  });
+};
+
+exports.getOuter = (req, res, next) => {
+  res.status(200).json({
+    title: "아우터",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "겉옷1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "겉옷2",
+      },
+    ],
+  });
+};
+
+exports.getTop = (req, res, next) => {
+  res.status(200).json({
+    title: "상의",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "상의1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "상의2",
+      },
+    ],
+  });
+};
+
+exports.getDress = (req, res, next) => {
+  res.status(200).json({
+    title: "원피스/치마",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "원피스",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "치마",
+      },
+    ],
+  });
+};
+
+exports.getBottom = (req, res, next) => {
+  res.status(200).json({
+    title: "하의",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "하의1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "하의2",
+      },
+    ],
+  });
+};
+
+exports.getBasic = (req, res, next) => {
+  res.status(200).json({
+    title: "베이직",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "베이직1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "베이직2",
+      },
+    ],
+  });
+};
+
+exports.getSet = (req, res, next) => {
+  res.status(200).json({
+    title: "세트",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "세트1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "세트2",
+      },
+    ],
+  });
+};
+
+exports.getSale = (req, res, next) => {
+  res.status(200).json({
+    title: "세일",
+    results: [
+      {
+        card__imgUrl: "https://picsum.photos/id/233/350/350",
+        card_title: "세일1",
+      },
+      {
+        card__imgUrl: "https://picsum.photos/id/253/350/350",
+        card_title: "세일2",
       },
     ],
   });
--- routes/cate.js
+++ routes/cate.js
@@ -4,6 +4,15 @@ const cateController = require("../controllers/cate");
 
 const router = express.Router();
 
-router.get("/best", cateController.getPosts);
+router.get("/best", cateController.getBest);
+router.get("/new", cateController.getNew);
+router.get("/basic", cateController.getBasic);
+router.get("/outer", cateController.getOuter);
+router.get("/top", cateController.getTop);
+router.get("/dress", cateController.getDress);
+router.get("/bottom", cateController.getBottom);
+router.get("/basic", cateController.getBasic);
+router.get("/set", cateController.getSet);
+router.get("/sale", cateController.getSale);
 
 module.exports = router;
